833db901008afe43c2fddbd1dffd05fea37dcd2a
misostack <misostack.com@gmail.com>
2020-11-06 12:00:15 +0700

    Add alias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ErrorService } from './services/error.service';
+import { ErrorService } from '@core/services';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [
     {
       provide: ErrorHandler,
       useClass: ErrorService,
-    }
+    },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
